b207eea3de9e7384facc857ce68dbd0c86274227
Kalycito <info@kalycito.com>
2010-09-06 07:41:01 +0000

    [FIX] Cleanup APIOperations.cpp
    * Remove unwanted commented codes
    * Comment the calling of fn UpdateMNNodeAssignmentIndex(), it deletes subindex which does not have a value
    * Correct the makefiles
    * Fix copy path in compile_Wrapper.sh

--- openCONFIGURATORSoln/openCOnfiguratorWrapper/compile_Wrapper.sh
+++ openCONFIGURATORSoln/openCOnfiguratorWrapper/compile_Wrapper.sh
@@ -2,6 +2,6 @@
 
 #g++ -shared -isystem /usr/include/tcl8.4 `xml2-config --cflags` Tcl_WrapperMain.cpp Tcl_WrapperMain_wrap.cxx ./openCONFIGURATOR.so -o Tcl_WrapperMain.so `xml2-config --libs`
 
-g++ -fPIC -shared -isystem /usr/include/tcl8.4 `xml2-config --cflags` Tcl_WrapperMain_wrap.cxx /usr/lib/openCONFIGURATOR.so -o openConfiguratorWrapper.so `xml2-config --libs`
-cp -pv *.so ./../../../GUI/openCONFIGURATOR/
-cp -pv /usr/lib/openCONFIGURATOR.so ./../../../GUI/openCONFIGURATOR/
+g++ -fPIC -shared -isystem /usr/include/tcl8.4 `xml2-config --cflags` openconfiguratorWrapper_wrap.cxx /usr/lib/openCONFIGURATOR.so -o openConfiguratorWrapper.so `xml2-config --libs`
+cp -pv *.so ./../../../../Application/Src/openCONFIGURATOR/
+sudo cp -pv /usr/lib/openCONFIGURATOR.so ./../../../../Application/Src/openCONFIGURATOR/
